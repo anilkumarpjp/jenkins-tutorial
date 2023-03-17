f6dc1c30797b2b60a6a1950d683b64f9cf6a414a
Anil Kumar A <anilkumar.parjapat169@gmail.com>
2023-03-17 11:45:32 +0530

    initial project commit

---
+++ src/test/java/com/techanything/jenkin/api/SpringJenkinsAppTest.java
@@ -0,0 +1,23 @@
+package com.techanything.jenkin.api;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJenkinsAppTest {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsAppTest.class);
+
+	@Test
+	public void contextLoads() {
+		logger.info("Test case executing...");
+		assertEquals(true, true);
+	}
+}
